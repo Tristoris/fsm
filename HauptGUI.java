914c0850f58c3eb129ac6809f0624932486a5d19
Matteo <74454734+matteokosina@users.noreply.github.com>
2023-01-15 17:20:51 +0100

    Added file-open-dialog

--- HauptGUI.java
+++ HauptGUI.java
@@ -7,6 +7,13 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.SpinnerValueFactory;
 import javafx.scene.control.Label;
+import javafx.stage.FileChooser.ExtensionFilter;
+import javafx.stage.FileChooser;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.Group;
+import java.io.File;
 
 // import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -20,18 +27,27 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView; 
 import javafx.geometry.Pos;
 
+
 public class HauptGUI extends Application {
     private Controller c;
     private Stage primaryStage;
     private ImageView banner;
     private TextField tf;
     private TextField tfd;
-
+    private Menu menu;
+    private MenuItem openBtn;
     private Button bLos;
+    private FileChooser dateidialog;
 
     @Override
     public void start(Stage primaryStage) {
         try {
+             dateidialog = new FileChooser();
+            // dateidialog.setInitialDirectory(new File("texte"));
+            dateidialog.getExtensionFilters().clear();
+            dateidialog.getExtensionFilters().add(new ExtensionFilter("XML-Dateien (*.xml)", "*.xml"));
+            dateidialog.getExtensionFilters().add(new ExtensionFilter("Json-Dateien (*.json)", "*.json"));
+           
             Controller c = new Controller(this);
             this.primaryStage = primaryStage;
             
@@ -44,7 +60,52 @@ public class HauptGUI extends Application {
             VBox haupt = new VBox();
             VBox uiField = new VBox();
             VBox buttonBox = new VBox();
+                // create a menu
+        Menu m = new Menu("Import");
+  
+        // create menuitems
+        MenuItem m1 = new MenuItem("open");
+        
+  
+        // add menu items to menu
+        m.getItems().add(m1);
+        
+       
+  
+        // create events for menu items
+        // action event
+        EventHandler<ActionEvent> event2 = new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent e)
+            {
+              File file = dateidialog.showOpenDialog(null);
+            if (file != null) {
+                System.out.println("Pfad:" + file.getAbsolutePath());
+                Automat automat = new Automat();
+                final int wordlength = file.getAbsolutePath().length() -1;
+                final char lastLetter = file.getAbsolutePath().charAt(wordlength);
+                
+                if(lastLetter == 'l'){
+                    automat.parseXMLtoAutomat(file.getAbsolutePath());
+                }else{
+                    automat.parseJSONtoAutomat(file.getAbsolutePath());
+                }
+                AppGUI app = new AppGUI(automat);
+                app.start(new Stage());
+            }
+            }
+        };
+  
+        // add event
+        m1.setOnAction(event2);
+      
+  
+        // create a menubar
+        MenuBar mb = new MenuBar();
+  
+        // add menu to menubar
+        mb.getMenus().add(m);
             
+        haupt.getChildren().add(mb);
             // set initial value for inputs
             final int initialValue = 1;
 
@@ -95,7 +156,7 @@ public class HauptGUI extends Application {
                     Platform.exit();            // Beendet
                     System.exit(0);
                 });
-            primaryStage.setAlwaysOnTop(true);
+            primaryStage.setAlwaysOnTop(false);
             primaryStage.show();
         } 
         catch(Exception e)    {
